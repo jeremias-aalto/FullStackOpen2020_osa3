58181e5b6cf7968a9fb5f845d98b928a6135c551
jeremias-aalto <jeremias.jokila@aalto.fi>
2020-10-30 02:29:30 +0200

    osa 3 valmis

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 require('dotenv').config()
 const Person = require('./models/person')
 
-morgan.token('info', function(req, res) { 
+morgan.token('info', function(req) {
   if (req.body.name) {
     return(JSON.stringify(req.body))
   }
@@ -29,28 +29,30 @@ app.use(
 
 app.use(cors())
 
-let persons = [  
-  {    
-    id: 1,    
-    name: "Arto Hellas",
-    number: "040-123456"
+/*
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
   },
-  {    
-    id: 2,    
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
   },
-  {    
-    id: 3,    
-    name: "Dan Abramov",
-    number: "12-43-234345"
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
   },
-  {    
-    id: 4,    
-    name: "Mary Poppendick",
-    number: "39-23-6423122"
+  {
+    id: 4,
+    name: 'Mary Poppendick',
+    number: '39-23-6423122'
   }
 ]
+*/
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -84,9 +86,7 @@ app.get('/api/persons/:id', (req, res, next) => {
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findOneAndRemove({ id: Number(req.params.id) })
-    .then(result => {
-      res.status(204).end()
-    })
+    .then(res.status(204).end())
     .catch(error => next(error))
 })
 
@@ -132,125 +132,3 @@ const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-
-
-
-/*
-
-// Heroku repo v1
-
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const app = express()
-
-morgan.token('info', function(req, res) { 
-  if (req.body.name) {
-    return(JSON.stringify(req.body))
-  }
-})
-
-app.use(express.static('build'))
-app.use(express.json())
-app.use(
-  morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens.info(req, res),
-    ].join(' ')
-  })
-)
-app.use(cors())
-
-let persons = [  
-  {    
-    id: 1,    
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {    
-    id: 2,    
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {    
-    id: 3,    
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {    
-    id: 4,    
-    name: "Mary Poppendick",
-    number: "39-23-6423122"
-  }
-]
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/info', (req, res) => {
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people.<br/>${new Date()}</p>`
-  )
-})
-
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
-})
-
-const newId = () => {
-  return (
-    Math.floor(Math.random() * Math.floor(250))
-  )
-}
-
-app.post('/api/persons', (request, response) => {
-  const body = request.body
-  if (!body.name||!body.number) {
-    return response.status(400).json({
-      error: 'name or number missing'
-    })
-  } else if (persons.filter(person => person.name === body.name).length > 0) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-  const person = {
-    id: newId(),
-    name: body.name,
-    number: body.number
-  }
-  persons = persons.concat(person)
-  response.json(person)
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
-*/
